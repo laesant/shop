e6c4ea999228cdaea202c066bf7ba06c0a454de6
Laênio Marques <105957017+laesant@users.noreply.github.com>
2023-10-23 13:55:08 -0300

    Navigating to product detail

--- lib/components/product_item.dart
+++ lib/components/product_item.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shop/models/product.dart';
+import 'package:shop/pages/product_detail_page.dart';
+import 'package:shop/utils/app_routes.dart';
 
 class ProductItem extends StatelessWidget {
   const ProductItem({super.key, required this.product});
@@ -24,9 +26,13 @@ class ProductItem extends StatelessWidget {
               onPressed: () {},
               icon: const Icon(Icons.shopping_cart)),
         ),
-        child: Image.network(
-          product.imageUrl,
-          fit: BoxFit.cover,
+        child: GestureDetector(
+          onTap: () => Navigator.of(context)
+              .pushNamed(AppRoutes.productDetail, arguments: product),
+          child: Image.network(
+            product.imageUrl,
+            fit: BoxFit.cover,
+          ),
         ),
       ),
     );
