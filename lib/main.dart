8730e3c76a18e975bf7891f5deed506818958374
Laênio Marques <105957017+laesant@users.noreply.github.com>
2023-10-26 11:51:29 -0300

    using provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/models/product_list.dart';
 import 'package:shop/pages/product_detail_page.dart';
 import 'package:shop/pages/products_overview_page.dart';
 import 'package:shop/utils/app_routes.dart';
@@ -12,17 +14,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Flutter Demo',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-            appBarTheme: const AppBarTheme(centerTitle: true),
-            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-            useMaterial3: true,
-            fontFamily: 'Lato'),
-        routes: {
-          AppRoutes.productDetail: (context) => const ProductDetailPage()
-        },
-        home: ProductsOverviewPage());
+    return ChangeNotifierProvider(
+      create: (_) => ProductList(),
+      child: MaterialApp(
+          title: 'Flutter Demo',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+              appBarTheme: const AppBarTheme(centerTitle: true),
+              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+              useMaterial3: true,
+              fontFamily: 'Lato'),
+          routes: {
+            AppRoutes.productDetail: (context) => const ProductDetailPage()
+          },
+          home: ProductsOverviewPage()),
+    );
   }
 }
