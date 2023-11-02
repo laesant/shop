dc5db733fec6f04ef448fc834495b7317799a2fc
Laênio Marques <105957017+laesant@users.noreply.github.com>
2023-11-02 15:41:03 -0300

    product details

--- lib/pages/product_detail_page.dart
+++ lib/pages/product_detail_page.dart
@@ -11,9 +11,35 @@ class ProductDetailPage extends StatelessWidget {
     final Product product =
         ModalRoute.of(context)!.settings.arguments as Product;
     return Scaffold(
-      appBar: AppBar(
-        title: Text(product.name),
-      ),
-    );
+        appBar: AppBar(
+          title: Text(product.name),
+        ),
+        body: SingleChildScrollView(
+            child: Column(
+          children: [
+            Container(
+              height: 300,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: NetworkImage(product.imageUrl),
+                      fit: BoxFit.cover)),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              "R\$ ${product.price}",
+              style: const TextStyle(color: Colors.grey, fontSize: 20),
+            ),
+            const SizedBox(height: 10),
+            Container(
+              width: double.infinity,
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Text(
+                product.description,
+                textAlign: TextAlign.center,
+              ),
+            )
+          ],
+        )));
   }
 }
