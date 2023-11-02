76cbb6f9ea37d9e00d91441fc673b135c5f698eb
Laênio Marques <105957017+laesant@users.noreply.github.com>
2023-11-02 15:58:46 -0300

    navigate with drawer

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:shop/models/cart.dart';
 import 'package:shop/models/order_list.dart';
 import 'package:shop/models/product_list.dart';
 import 'package:shop/pages/cart_page.dart';
+import 'package:shop/pages/orders_page.dart';
 import 'package:shop/pages/product_detail_page.dart';
 import 'package:shop/pages/products_overview_page.dart';
 import 'package:shop/utils/app_routes.dart';
@@ -30,18 +31,20 @@ class MyApp extends StatelessWidget {
         )
       ],
       child: MaterialApp(
-          title: 'Flutter Demo',
-          debugShowCheckedModeBanner: false,
-          theme: ThemeData(
-              appBarTheme: const AppBarTheme(centerTitle: true),
-              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-              useMaterial3: true,
-              fontFamily: 'Lato'),
-          routes: {
-            AppRoutes.productDetail: (context) => const ProductDetailPage(),
-            AppRoutes.cart: (context) => const CartPage()
-          },
-          home: const ProductsOverviewPage()),
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+            appBarTheme: const AppBarTheme(centerTitle: true),
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            useMaterial3: true,
+            fontFamily: 'Lato'),
+        routes: {
+          AppRoutes.home: (context) => const ProductsOverviewPage(),
+          AppRoutes.productDetail: (context) => const ProductDetailPage(),
+          AppRoutes.cart: (context) => const CartPage(),
+          AppRoutes.orders: (context) => const OrdersPage()
+        },
+      ),
     );
   }
 }
--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shop/components/app_drawer.dart';
 import 'package:shop/components/product_grid.dart';
 import 'package:shop/models/cart.dart';
 import 'package:shop/utils/app_routes.dart';
@@ -63,6 +64,7 @@ class _ProductsOverviewPageState extends State<ProductsOverviewPage> {
           ),
         ],
       ),
+      drawer: const AppDrawer(),
       body: ProductGrid(showFavoriteOnly: _showFavoriteOnly),
     );
   }
