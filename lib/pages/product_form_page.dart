ff70ce0f305ee2f0e1977db8f98c1ad50df040e9
Laênio Marques <105957017+laesant@users.noreply.github.com>
2023-11-06 10:53:35 -0300

    controlling the focus

--- lib/pages/product_form_page.dart
+++ lib/pages/product_form_page.dart
@@ -8,12 +8,42 @@ class ProductFormPage extends StatefulWidget {
 }
 
 class _ProductFormPageState extends State<ProductFormPage> {
+  late final FocusNode _priceFocus;
+  @override
+  void initState() {
+    _priceFocus = FocusNode();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _priceFocus.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Formulário de Produto"),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text("Formulário de Produto"),
+        ),
+        body: Form(
+            child: ListView(
+          padding: const EdgeInsets.all(15),
+          children: [
+            TextFormField(
+              textInputAction: TextInputAction.next,
+              decoration: const InputDecoration(labelText: "Nome"),
+              onFieldSubmitted: (_) => _priceFocus.requestFocus(),
+            ),
+            TextFormField(
+              focusNode: _priceFocus,
+              textInputAction: TextInputAction.next,
+              decoration: const InputDecoration(labelText: "Preço"),
+              keyboardType:
+                  const TextInputType.numberWithOptions(decimal: true),
+            )
+          ],
+        )));
   }
 }
