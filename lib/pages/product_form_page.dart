b829a3ea95dae94b734899482db590658d882f98
Laênio Marques <105957017+laesant@users.noreply.github.com>
2023-11-06 11:32:20 -0300

    input field url

--- lib/pages/product_form_page.dart
+++ lib/pages/product_form_page.dart
@@ -10,19 +10,29 @@ class ProductFormPage extends StatefulWidget {
 class _ProductFormPageState extends State<ProductFormPage> {
   late final FocusNode _priceFocus;
   late final FocusNode _descriptionFocus;
+  late final FocusNode _imageUrlFocus;
+  late final TextEditingController _imageUrlController;
   @override
   void initState() {
     _priceFocus = FocusNode();
     _descriptionFocus = FocusNode();
+    _imageUrlFocus = FocusNode();
+    _imageUrlController = TextEditingController();
+    _imageUrlFocus.addListener(updateImage);
     super.initState();
   }
 
   @override
   void dispose() {
+    _imageUrlFocus.removeListener(updateImage);
     _priceFocus.dispose();
     _descriptionFocus.dispose();
+    _imageUrlFocus.dispose();
+    _imageUrlController.dispose();
     super.dispose();
   }
+
+  void updateImage() => setState(() {});
 
   @override
   Widget build(BuildContext context) {
@@ -49,10 +59,42 @@ class _ProductFormPageState extends State<ProductFormPage> {
             ),
             TextFormField(
               focusNode: _descriptionFocus,
-              textInputAction: TextInputAction.next,
+              textInputAction: TextInputAction.newline,
               decoration: const InputDecoration(labelText: "Descrição"),
               keyboardType: TextInputType.multiline,
               maxLines: 3,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Expanded(
+                  child: TextFormField(
+                    focusNode: _imageUrlFocus,
+                    decoration:
+                        const InputDecoration(labelText: "Url da Imagem"),
+                    keyboardType: TextInputType.url,
+                    textInputAction: TextInputAction.done,
+                    controller: _imageUrlController,
+                    onChanged: (_) => updateImage(),
+                  ),
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  alignment: Alignment.center,
+                  margin: const EdgeInsets.only(top: 10, left: 10),
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey, width: 1),
+                      image: _imageUrlController.text.isNotEmpty
+                          ? DecorationImage(
+                              fit: BoxFit.cover,
+                              image: NetworkImage(_imageUrlController.text))
+                          : null),
+                  child: _imageUrlController.text.isEmpty
+                      ? const Text('Informe a Url')
+                      : null,
+                )
+              ],
             ),
           ],
         )));
