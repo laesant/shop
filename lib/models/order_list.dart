351c09fd5c2c47908607c1eb230afd680d643324
Laênio Marques <105957017+laesant@users.noreply.github.com>
2023-11-10 12:25:49 -0300

    load orders from back-end

--- lib/models/order_list.dart
+++ lib/models/order_list.dart
@@ -4,6 +4,7 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:shop/models/cart.dart';
+import 'package:shop/models/cart_item.dart';
 import 'package:shop/models/order.dart';
 import 'package:shop/utils/constants.dart';
 
@@ -13,6 +14,28 @@ class OrderList with ChangeNotifier {
   List<Order> get items => [..._items];
 
   int get itemsCount => _items.length;
+
+  Future<void> loadOrders() async {
+    _items.clear();
+    var response = await http.get(Uri.parse("${Constants.orderBaseUrl}.json"));
+    if (response.body == 'null') return;
+    Map<String, dynamic> data = jsonDecode(response.body);
+    data.forEach((key, value) {
+      _items.add(Order(
+          id: key,
+          total: value['total'],
+          products: List<CartItem>.from(value['products']
+              .map((cartItem) => CartItem(
+                  id: cartItem['id'],
+                  productId: cartItem['productId'],
+                  name: cartItem['name'],
+                  quantity: cartItem['quantity'],
+                  price: cartItem["price"]))
+              .toList()),
+          date: DateTime.parse(value['date'])));
+    });
+    notifyListeners();
+  }
 
   Future<void> addOrder(Cart cart) async {
     final date = DateTime.now();
