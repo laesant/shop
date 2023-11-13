43e15434e87ad7d3079ea489887d8b4a43f7fa49
Laênio Marques <105957017+laesant@users.noreply.github.com>
2023-11-13 11:23:53 -0300

    auth page #2

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -1,4 +1,4 @@
-import 'dart:ui';
+import 'dart:math';
 
 import 'package:flutter/material.dart';
 
@@ -12,16 +12,44 @@ class AuthPage extends StatelessWidget {
       children: [
         Container(
           decoration: const BoxDecoration(
-              gradient: LinearGradient(colors: [
-            Color.fromRGBO(215, 117, 255, 0.5),
-            Color.fromRGBO(255, 188, 117, 0.9)
-          ])),
+              gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [
+                Color.fromRGBO(215, 117, 255, 0.5),
+                Color.fromRGBO(255, 188, 117, 0.9)
+              ])),
         ),
         Container(
           width: double.infinity,
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              
+              Container(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 70),
+                // cascade operator (..)
+                transform: Matrix4.rotationZ(-8 * pi / 180)..translate(-10.0),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.deepOrange.shade900,
+                    boxShadow: const [
+                      BoxShadow(
+                          blurRadius: 8,
+                          color: Colors.black26,
+                          offset: Offset(0, 2))
+                    ]),
+                child: Text(
+                  "Minha Loja",
+                  style: TextStyle(
+                      fontSize: 45,
+                      fontFamily: 'Anton',
+                      color: Theme.of(context)
+                          .primaryTextTheme
+                          .headlineMedium!
+                          .color),
+                ),
+              )
             ],
           ),
         )
