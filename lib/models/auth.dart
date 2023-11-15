bcd69890085aef82d9ab00733906a9524bfa23f4
Laênio Marques <105957017+laesant@users.noreply.github.com>
2023-11-15 12:02:58 -0300

    auto login

--- lib/models/auth.dart
+++ lib/models/auth.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:shop/data/store.dart';
 import 'package:shop/exceptions/auth_exception.dart';
 
 class Auth with ChangeNotifier {
@@ -46,6 +47,12 @@ class Auth with ChangeNotifier {
       _uid = body['localId'];
       _expiryDate =
           DateTime.now().add(Duration(seconds: int.parse(body['expiresIn'])));
+      Store.saveMap('userData', {
+        'token': _token,
+        'email': _email,
+        'userId': _uid,
+        'expiryDate': _expiryDate!.toIso8601String()
+      });
       _autoLogout();
       notifyListeners();
     }
@@ -58,12 +65,32 @@ class Auth with ChangeNotifier {
       _authenticate(
           email: email, password: password, urlFragment: 'signInWithPassword');
 
-  void logout() {
+  Future<void> tryAutoLogin() async {
+    if (isAuth) return;
+
+    final Map<String, dynamic> userData = await Store.getMap('userData');
+
+    if (userData.isEmpty) return;
+
+    final expiryDate = DateTime.parse(userData['expiryDate']);
+
+    if (expiryDate.isBefore(DateTime.now())) return;
+
+    _token = userData['token'];
+    _email = userData['email'];
+    _uid = userData['userId'];
+    _expiryDate = expiryDate;
+    _autoLogout();
+    notifyListeners();
+  }
+
+  void logout() async {
     _token = null;
     _uid = null;
     _email = null;
     _expiryDate = null;
     _clearLogoutTimer();
+    await Store.remove('userData');
     notifyListeners();
   }
 
--- lib/pages/auth_or_home_page.dart
+++ lib/pages/auth_or_home_page.dart
@@ -10,6 +10,26 @@ class AuthOrHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Auth auth = Provider.of(context);
-    return auth.isAuth ? const ProductsOverviewPage() : const AuthPage();
+
+    return FutureBuilder(
+        future: auth.tryAutoLogin(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const ColoredBox(
+              color: Colors.white,
+              child:  Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          } else if (snapshot.error != null) {
+            return const Center(
+              child: Text("Ocorreu um erro!"),
+            );
+          } else {
+            return auth.isAuth
+                ? const ProductsOverviewPage()
+                : const AuthPage();
+          }
+        });
   }
 }
